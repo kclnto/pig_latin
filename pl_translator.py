8246af1f5188fe96316a6dc07f39d2f05fef21d9
Kacy T <41276326+kclnto@users.noreply.github.com>
2022-07-05 16:31:34 -0700

    Update pl_translator.py

--- pl_translator.py
+++ pl_translator.py
@@ -1,11 +1,14 @@
 def pig_latin(text):
   #Separate the text into words
   words = text.split(" ")
+  
   pl_words = []
+  
   for word in words:
     # Create the pig latin word and add it to the list
     word = word[1:] + word[0] + "ay"
     pl_words.append(word)
+  
   # Turn the list back into a phrase
-  new_text = " ".join(pl_words)
-  return new_text
+  pl_text = " ".join(pl_words)
+  return pl_text
